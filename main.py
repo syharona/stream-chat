c5feb883d6c2fe6c522fb9809934585f68b9b2ab
user <syharona@gmail.com>
2023-06-18 19:55:51 +0100

    Final commit with session state memory and history

---
+++ main.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from streamlit_chat import message
+from dotenv import load_dotenv
+import os
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+
+def init():
+    load_dotenv()
+
+    #Load OpenAI API key
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY")=="":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+
+
+    st.set_page_config(
+        page_title="App For Stream Testing",
+        page_icon='🤖'
+    )
+
+
+def main():
+    init()
+
+    st.header("Your neighbour friendly ChatGPT 🤖")
+
+    chat = ChatOpenAI(temperature=0)
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="You are a helpful and playful bot whose purpose is to explain complex concepts like you are talking to five year olds"),
+        ]
+
+    with st.sidebar:
+        user_input=st.text_input("Your message to the bot: ", key="user_input")
+
+        if user_input:
+            st.session_state.messages.append(HumanMessage(content=user_input))
+            with st.spinner("Thinking..."):
+                response=chat(st.session_state.messages)
+            st.session_state.messages.append(AIMessage(content=response.content))
+    
+
+    messages = st.session_state.get('messages',[])
+    
+    for i,msg in enumerate(messages[1:]):
+        if i % 2 == 0:
+            message(msg.content, is_user=True, key=str(i) + '_user')
+        else:
+            message(msg.content, is_user=False, key=str(i) + '_ai')
+
+    
+
+
+
+if __name__=='__main__':
+    main()
